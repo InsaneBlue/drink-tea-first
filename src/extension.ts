12de65bf5910a697b7817f1f71e53f8e84152378
InsaneBlue <469045872@qq.com>
2021-07-16 23:25:49 +0800

    test: message

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('drink-tea-first.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('You Must Drink Tea First!');
+		vscode.window.showInformationMessage('Drink Tea First!');
 	});
 
 	context.subscriptions.push(disposable);
