3bb9e8c6d2c7059362c058ca13ab8c6f6cb756f8
InsaneBlue <469045872@qq.com>
2021-08-10 00:33:50 +0800

    feat: add daily config

--- src/service/dailyReminder.ts
+++ src/service/dailyReminder.ts
@@ -1,23 +1,15 @@
 import * as dayjs from "dayjs";
-
-// 默认任务
-// 15点饮茶 19点下班
-const defaultTask = [
-  {
-    activeTime: "09:00:00",
-    message: "喂！三点几啦，饮茶先啦！",
-  },
-  {
-    activeTime: "14:45:00",
-    message: "喂！快三点啦，看看基金股票啦！",
-  },
-  {
-    activeTime: "19:00:00",
-    message: "喂！朋友！差不多七点多啦，喝杯茶happy下，下班啦！",
-  },
-];
+import { getConfiguration } from "./utils";
+import * as vscode from "vscode";
 
 function dailyReminder() {
+  // 读取默认任务配置
+  const defaultTask = getConfiguration().get("drink.daily.task", []);
+  // 读取默认提示日配置
+  const defaultDay: any = vscode.workspace
+    .getConfiguration()
+    .get("drink.daily.day", []);
+
   // 定时器触发前检查是否需要插入任务
   function onTimerTrigger(timer: any, params: any) {
     const { triggerTimeTs } = params;
@@ -25,11 +17,20 @@ function dailyReminder() {
       options: { interval },
     } = timer;
 
-    const task = defaultTask.reduce((acc: any, cur) => {
+    const task = defaultTask.reduce((acc: any, cur: any) => {
       const { activeTime } = cur;
       const str = `${dayjs(triggerTimeTs).format("YYYY-MM-DD")} ${activeTime}`;
       const taskTimeTs = +new Date(str);
 
+      // 不在预设日期范围内不提示
+      const curDay = dayjs(triggerTimeTs).day();
+      const isInRemindDay = defaultDay.includes(curDay);
+
+      if (!isInRemindDay) {
+        return;
+      }
+
+      // 当任务时间在当前时间间隔中时，在加入任务队列
       if (
         taskTimeTs >= triggerTimeTs &&
         taskTimeTs < triggerTimeTs + interval
--- src/service/utils.ts
+++ src/service/utils.ts
@@ -1,4 +1,11 @@
-// is function type
+import * as vscode from "vscode";
+
+// 判断是否是函数
 export function isFunction(fun: any) {
   return typeof fun === "function";
+}
+
+// 读取插件配置
+export function getConfiguration() {
+  return vscode.workspace.getConfiguration();
 }
